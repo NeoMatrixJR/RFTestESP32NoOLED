7d754ce17366f2a02a659543f70238c8fdb59b5a
Justin Richeson <NeoMatrixJR@gmail.com>
2024-05-21 00:11:58 -0500

    Using Arduino IDE Example

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,93 @@
+/**
+   ESPNOW - Basic communication - Slave
+   Date: 26th September 2017
+   Author: Arvind Ravulavaru <https://github.com/arvindr21>
+   Purpose: ESPNow Communication between a Master ESP32 and a Slave ESP32
+   Description: This sketch consists of the code for the Slave module.
+   Resources: (A bit outdated)
+   a. https://espressif.com/sites/default/files/documentation/esp-now_user_guide_en.pdf
+   b. http://www.esploradores.com/practica-6-conexion-esp-now/
+
+   << This Device Slave >>
+
+   Flow: Master
+   Step 1 : ESPNow Init on Master and set it in STA mode
+   Step 2 : Start scanning for Slave ESP32 (we have added a prefix of `slave` to the SSID of slave for an easy setup)
+   Step 3 : Once found, add Slave as peer
+   Step 4 : Register for send callback
+   Step 5 : Start Transmitting data from Master to Slave
+
+   Flow: Slave
+   Step 1 : ESPNow Init on Slave
+   Step 2 : Update the SSID of Slave with a prefix of `slave`
+   Step 3 : Set Slave in AP mode
+   Step 4 : Register for receive callback and wait for data
+   Step 5 : Once data arrives, print it in the serial monitor
+
+   Note: Master and Slave have been defined to easily understand the setup.
+         Based on the ESPNOW API, there is no concept of Master and Slave.
+         Any devices can act as master or salve.
+*/
+
 #include <Arduino.h>
+#include <esp_now.h>
 #include <WiFi.h>
-#include <ESPNowW.h>
 
-// ESPNow Setup
-#define PEER_ADDRESS "B4:E6:2D:8E:5D:3D" // Replace with the MAC address of the OLED board
+#define CHANNEL 1
 
-uint32_t last_send_time = 0;
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
 
-void onRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len);
+// config AP SSID
+void configDeviceAP() {
+  const char *SSID = "Slave_1";
+  bool result = WiFi.softAP(SSID, "Slave_1_Password", CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+    Serial.print("AP CHANNEL "); Serial.println(WiFi.channel());
+  }
+}
+
+// callback when data is recv from Master
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  Serial.print("Last Packet Recv Data: "); Serial.println(*data);
+  Serial.println("");
+}
 
 void setup() {
   Serial.begin(115200);
-  Serial.printf("Setting up Bouncer\n");
-  Serial.print("MAC: ");
-  Serial.println(WiFi.macAddress());
-  // Initialize ESPNow
-  WiFi.mode(WIFI_STA); // MUST NOT BE WIFI_MODE_NULL
-  WiFi.disconnect();
-  ESPNow.init();
-  ESPNow.add_peer((uint8_t *)PEER_ADDRESS);
-  ESPNow.reg_recv_cb(onRecv);
-  Serial.println("Setup Complete - Listening...");
+  Serial.println("ESPNow/Basic/Slave Example");
+  //Set device in AP mode to begin with
+  WiFi.mode(WIFI_AP);
+  // configure device AP mode
+  configDeviceAP();
+  // This is the mac address of the Slave in AP Mode
+  Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info.
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop() {
-  // Send packet every 5 seconds
-  static uint32_t now = millis();
-  if (now - last_send_time >= 5000) {
-    last_send_time = now;
-    uint8_t packet[] = "Hello!";
-    Serial.println("Sending data...");
-    int status = ESPNow.send_message((uint8_t *)PEER_ADDRESS, packet, sizeof(packet));
-    Serial.print("Send status: ");
-    Serial.println(status);
-    Serial.println("Data sent!");
-  }
-}
-
-void onRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
-  Serial.println("Received data!");
-  char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4],
-           mac_addr[5]);
-  Serial.print("Last Packet Recv from: ");
-  Serial.println(macStr);
-  Serial.print("Last Packet Recv Data: ");
-  // if it could be a string, print as one
-  if (data[data_len - 1] == 0)
-    Serial.printf("%s\n", data);
-  // additionally print as hex
-  for (int i = 0; i < data_len; i++) {
-    Serial.printf("%x ", data[i]);
-  }
-  Serial.println("");
+  // Chill
 }
